d845d9f1799b327b74cba05fc6677e601d918455
JackMarsh5 <159877430+JackMarsh5@users.noreply.github.com>
2025-05-23 12:50:17 -0700

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # entry.sh - Launch Amiga Xbox Direct Controller
 
-cd /mnt/managed_home/farm-ng-user-jmarsh/amiga-xbox-direct
+cd /mnt/managed_home/farm-ng-user-jmarsh/AmigaXboxDirect
 source venv/bin/activate
-python3 -m amiga_xbox_direct.main
+python3 -m AmigaXboxDirect.main

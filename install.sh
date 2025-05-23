cd00f7818f1eb3c255413aa1d4d11bd357bad367
JackMarsh5 <159877430+JackMarsh5@users.noreply.github.com>
2025-05-23 13:33:17 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
-#! /bin/bash
-
+#!/bin/bash
 set -uxeo pipefail
 
+# Get full path to this repo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-rm -f ~/manifest.json
-ln -s "$DIR/manifest.json" ~/manifest.json
+# Create launcher manifest directory if it doesn't exist
+mkdir -p ~/.farm-ng/manifest.d
+
+# Symlink this app's manifest.json into the shared directory
+ln -sf "$DIR/manifest.json" ~/.farm-ng/manifest.d/amiga-xbox-direct.json

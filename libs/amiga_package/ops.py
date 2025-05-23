7a9801f610f07e2114b80fe0a6853f8f58168d7e
JackMarsh5 <159877430+JackMarsh5@users.noreply.github.com>
2025-05-23 13:44:17 -0700

    Update ops.py

--- libs/amiga_package/ops.py
+++ libs/amiga_package/ops.py
@@ -1,8 +1,7 @@
 """Template module with math functions."""
 
-
 def add(a: int, b: int) -> int:
-    """Template function to add to integer values."""
-    assert isinstance(a, int), f"not an integer. Got: {type(a)}"
-    assert type(a) == type(b), f"Type of 'b' must be equal. Got: {type(b)}"
+    """Template function to add two integer values."""
+    assert isinstance(a, int), f"Argument 'a' is not an integer. Got: {type(a)}"
+    assert type(a) == type(b), f"Type of 'b' must match type of 'a'. Got: {type(b)}"
     return a + b

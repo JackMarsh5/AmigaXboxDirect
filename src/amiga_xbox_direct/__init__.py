defd4565fef04a6a25291de75dc9d0b7fb2b03e8
JackMarsh5 <159877430+JackMarsh5@users.noreply.github.com>
2025-05-23 17:09:57 -0700

    Update __init__.py

--- src/amiga_xbox_direct/__init__.py
+++ src/amiga_xbox_direct/__init__.py
@@ -1,5 +1,6 @@
-# ... inside your app initialization:
-service_cfg = EventServiceConfig.from_file("config.json")  # load our config with canbus details
-canbus_client = EventClient(service_cfg.service("canbus"))  # connect to canbus service
 from .event_client import EventClient, EventServiceConfig
+# ... inside your app initialization:
+service_cfg = EventServiceConfig.from_file("service_config.json")  # load our config with canbus details
+canbus_client = EventClient(service_cfg.service("canbus"))  # connect to canbus service
+
 

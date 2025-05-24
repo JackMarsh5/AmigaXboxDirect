5519e1924b369f2bfee1feac5941792fb7e94cdb
JackMarsh5 <159877430+JackMarsh5@users.noreply.github.com>
2025-05-23 17:32:56 -0700

    Update __init__.py

--- src/amiga_xbox_direct/__init__.py
+++ src/amiga_xbox_direct/__init__.py
@@ -1,3 +1,3 @@
-from farm_ng.core.event_client import EventClientConfig, EventClient
-event_client_config = EventClientConfig.from_file("service_config.json")
-canbus_client = EventClient(event_client_config.service("canbus"))
+#from farm_ng.core.event_client import EventClientConfig, EventClient
+#event_client_config = EventClientConfig.from_file("service_config.json")
+#canbus_client = EventClient(event_client_config.service("canbus"))

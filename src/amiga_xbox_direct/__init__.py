0993a84e4d293a8ccc2cc36d91e07e2a7abd8e23
JackMarsh5 <159877430+JackMarsh5@users.noreply.github.com>
2025-05-23 17:11:05 -0700

    Update __init__.py

--- src/amiga_xbox_direct/__init__.py
+++ src/amiga_xbox_direct/__init__.py
@@ -1,4 +1,5 @@
-from .event_client import EventClient, EventServiceConfig
+from farm_ng.core import EventClient, EventServiceConfig
+
 # ... inside your app initialization:
 service_cfg = EventServiceConfig.from_file("service_config.json")  # load our config with canbus details
 canbus_client = EventClient(service_cfg.service("canbus"))  # connect to canbus service
